690de6325fe8932aedddd18c31d5ee664c7e8ce3
waldemar_karpiel <karpiel.waldemar@gmail.com>
2018-05-23 13:54:08 +0200

    moved widget next to place order button / adjust colors depends on configuration

---
+++ app/code/Riskified/Decider/Controller/Deco/OptIn.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: teamcminds
+ * Date: 23/05/2018
+ * Time: 10:28
+ */
--- app/code/Riskified/Decider/view/frontend/web/js/eligible.js
+++ app/code/Riskified/Decider/view/frontend/web/js/eligible.js
@@ -6,29 +6,38 @@ define([
 ], function ($, ko, quote, storage) {
     'use strict';
 
-    return {
-        /**
-         *
-         * @return {*}
-         */
-        getIsEligible: function () {
-            return this.eligible = ko.observable();
-        },
+    function optInCall() {
+        storage.post(
+            'decider/deco/optIn',
+            JSON.stringify({
+                quote_id: quote.getQuoteId()
+            }),
+            true
+        ).done(function (result) {
 
-        paymentFail: function() {
-            var quoteId = quote.getQuoteId();
+        });
+    }
+
+    return {
+        paymentFail: function(buttonColor, buttonTextColor, logoUrl) {
             storage.post(
                 'decider/deco/isEligible',
                 JSON.stringify({
-                    quote_id: quoteId
+                    quote_id: quote.getQuoteId()
                 }),
                 true
             ).done(function (result) {
                 if (result.status == 'eligible') {
                     $('#deco-container').html("<div id='deco-widget'></div>");
-                    window.drawDecoWidget();
+                    window.drawDecoWidget(() => {
+                        return optInCall();
+                    }, {
+                        buttonColor: buttonColor,
+                        buttonText: buttonTextColor,
+                        logoUrl: logoUrl
+                    });
                 }
             });
         }
-    };
+    }
 });
