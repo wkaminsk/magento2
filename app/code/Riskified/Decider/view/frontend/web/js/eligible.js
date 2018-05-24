a5912eb6454690c1e26120cb0e1ab7797797504d
waldemar_karpiel <karpiel.waldemar@gmail.com>
2018-05-24 13:37:59 +0200

    added loader on js actions

--- app/code/Riskified/Decider/view/frontend/web/js/eligible.js
+++ app/code/Riskified/Decider/view/frontend/web/js/eligible.js
@@ -4,11 +4,13 @@ define([
     'Magento_Checkout/js/model/quote',
     'mage/storage',
     'Magento_Checkout/js/action/redirect-on-success',
-    'Magento_Checkout/js/checkout-data'
-], function ($, ko, quote, storage, redirectOnSuccessAction, checkoutData) {
+    'Magento_Checkout/js/checkout-data',
+    'Magento_Checkout/js/model/full-screen-loader'
+], function ($, ko, quote, storage, redirectOnSuccessAction, checkoutData, fullScreenLoader) {
     'use strict';
 
     function optInCall() {
+        fullScreenLoader.startLoader();
         storage.post(
             'decider/deco/optIn',
             JSON.stringify({
@@ -21,10 +23,12 @@ define([
                 redirectOnSuccessAction.execute();
             }
         });
+        fullScreenLoader.stopLoader();
     }
 
     return {
         paymentFail: function(buttonColor, buttonTextColor, logoUrl) {
+            fullScreenLoader.startLoader();
             storage.post(
                 'decider/deco/isEligible',
                 JSON.stringify({
@@ -41,8 +45,10 @@ define([
                         buttonText: buttonTextColor,
                         logoUrl: logoUrl
                     });
+                    $("#deco-main-button").click(function(e){e.preventDefault();});
                 }
             });
+            fullScreenLoader.stopLoader();
         }
     }
 });
